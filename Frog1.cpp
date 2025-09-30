f2200eb77032045d81e3fb90c710c46a9cb1c565
Rajnish Kumar <rj2537580@gmail.com>
2025-09-30 16:02:21 +0530

    add memoization in frog1 problem

--- Frog1.cpp
+++ Frog1.cpp
@@ -1,18 +1,62 @@
-#include<bits/stdc++.h>
+// PURE RECURSIVE APPROACH
+// #include<bits/stdc++.h>
 
+// using namespace std;
+
+// long long solve(vector<int>& h,int n)
+// {
+//         if(n == 0)return 0;
+
+//         long long cost1 = solve(h,n-1) + abs(h[n] - h[n-1]);
+
+//         if(n == 1)return cost1;
+
+//         long long cost2 = solve(h,n-2) + abs(h[n] - h[n-2]);
+
+//         return min(cost1, cost2);
+// }
+
+// int main()
+// {
+//     int N;
+//     cin >> N;
+//     vector<int> h(N);
+
+//     for(int i = 0; i < N; ++i)
+//     {
+//         cin >> h[i];
+//     }
+
+//     cout << solve(h,N-1) << "\n";
+//     return 0;
+// }
+
+//Recursion + memoization
+
+#include<bits/stdc++.h>
 using namespace std;
 
-long long solve(vector<int>& h,int n)
+long long solve(vector<int>& h,int n,vector<long long>& dp)
 {
-        if(n == 0)return 0;
+    if(n == 0)
+    {
+        return 0;
+    }
 
-        long long cost1 = solve(h,n-1) + abs(h[n] - h[n-1]);
+    if(dp[n] != -1)
+    {
+        return dp[n];
+    }
 
-        if(n == 1)return cost1;
+    long long cost1 = solve(h,n-1,dp) + abs(h[n]-h[n-1]);
 
-        long long cost2 = solve(h,n-2) + abs(h[n] - h[n-2]);
+    if(n == 1)
+    {
+        return dp[n] = cost1;
+    }
 
-        return min(cost1, cost2);
+    long long cost2 = solve(h,n-2,dp) + abs(h[n]-h[n-2]);
+    return dp[n] = min(cost1,cost2);
 }
 
 int main()
@@ -26,7 +70,9 @@ int main()
         cin >> h[i];
     }
 
-    cout << solve(h,N-1) << "\n";
+    vector<long long> dp(N,-1);
+
+    cout << solve(h, N-1,dp) << "\n";
+
     return 0;
 }
-
